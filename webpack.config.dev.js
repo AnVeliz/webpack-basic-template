bd0b49481598e0b51b8d5f8cf66ef03cb4ac7158
Anatoliy <Anatoliy.Velizhanin@gmail.com>
2021-04-03 18:18:07 +0500

    Extract babel config to a separate file, use Debug option for TypeScript

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,6 +6,7 @@ const basic = require('./webpack.config');
 
 module.exports = merge(basic, {
     mode: 'development',
+    devtool: 'source-map',
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
