f6f794113fee0284b133d49cc46d16721c7cf51d
Anatoliy <Anatoliy.Velizhanin@gmail.com>
2021-04-03 17:05:08 +0500

    Clean dist folder for production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const basic = require('./webpack.config');
 
@@ -9,5 +11,11 @@ module.exports = merge(basic, {
     output: {
         filename: '[name].[hash].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
 });
